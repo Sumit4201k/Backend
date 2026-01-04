3f15e8aaa8318aca6226ad7b63bb4ca4e6fb342d
Sumit4201k <kamtisumit6856@gmail.com>
2026-01-04 21:20:03 +0530

    Basic backendddand express js data flow understanding

--- 01_BasicExpressJS/index.js
+++ 01_BasicExpressJS/index.js
@@ -1,1 +1,15 @@
-console.log('hi there ');
+const express = require('express')
+require('dotenv').config()
+const app = express()
+// const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/google', (req, res) => {
+  res.send('www.google.com')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`)
+})
